6352858605244e356b402aa30c7c25ebd19cf2e4
wu546300070 <546300070@qq.com>
2022-01-15 16:49:34 +0800

    8

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm1')
     print_hi('PyCharm2')
     print_hi('PyCharm3')
+    print_hi('PyCharm4')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
